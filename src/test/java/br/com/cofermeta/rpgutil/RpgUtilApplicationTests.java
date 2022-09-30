fad2ca4b005b0b41680a7196b6055d02fb85959f
minhoad <darmes2000@hotmail.com>
2022-09-30 16:49:48 -0300

    Initial commit

---
+++ src/test/java/br/com/cofermeta/rpgutil/RpgUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cofermeta.rpgutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgUtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
